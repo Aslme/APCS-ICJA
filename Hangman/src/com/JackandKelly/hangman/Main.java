4c7a8dc548d5ae35773f97746abe4f5175d10558
kellyshapiro <kshaps22@gmail.com>
2020-01-09 13:52:53 -0600

    added hi jack

--- Hangman/src/com/JackandKelly/hangman/Main.java
+++ Hangman/src/com/JackandKelly/hangman/Main.java
@@ -1,10 +1,13 @@
 package com.JackandKelly.hangman;
 
+import java.util.concurrent.SynchronousQueue;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("HI KELLY");
+        System.out.println("HI Jack!");
+        System.out.println("Hi Jack");
     }
 }
