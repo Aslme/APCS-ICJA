1d78e10c1b4549002c8515c14f9c273f3722c553
HomemadeToast57 <54961512+HomemadeToast57@users.noreply.github.com>
2020-01-13 13:39:58 -0600

    Create Welcome.java

---
+++ Hangman/src/com/JackandKelly/hangman/Welcome.java
@@ -0,0 +1,13 @@
+package com.JackandKelly.hangman;
+
+public class Welcome {
+
+    public void welcomeMessage(){
+
+        System.out.println("Welcome to Hangman. This is a game where we will test your wordy skills.\n You will have to guess the letters to a mystery word");
+
+
+    }
+
+
+}
